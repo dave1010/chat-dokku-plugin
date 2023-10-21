ae355d45979a26df52e41f22064cd5d916e31512
Dave Hulbert <dave1010@gmail.com>
2023-10-21 17:47:21 +0100

    Write files

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from src.auth import auth_bp, check_auth
 from src.self_update import self_update_bp
-from src.exec_util import exec_as_chatdokku, exec_script_as_chatdokku
+from src.exec_util import exec_as_chatdokku, exec_script_as_chatdokku, scp_to_app, is_safe_path
 
 app = Flask(__name__)
 app.register_blueprint(auth_bp)
@@ -34,6 +34,27 @@ def generate_random_app_name():
 def app_create():
     app_name = request.args.get('app_name', generate_random_app_name())
     return jsonify(exec_script_as_chatdokku(f'create-app.sh {app_name}'))
+
+
+@app.route('/write-file', methods=['POST'])
+def write_file():
+    app_name = request.json.get('app_name')
+    if not app_name:
+        return {"error": "App name is required. Check what apps exist with app-list."}, 400
+
+    path = request.json.get('path')
+    if not path:
+        return {"error": "path is required"}, 400
+
+    if not is_safe_path(app_name, path):
+        return {"error": "invalid path"}, 400
+
+    contents = request.json.get('contents')
+    if not contents:
+        return {"error": "contents is required"}, 400
+
+    return jsonify(scp_to_app(file_contents=contents, app_name=app_name, path=path))
+
 
 
 if __name__ == '__main__':
--- src/exec_util.py
+++ src/exec_util.py
@@ -1,4 +1,11 @@
 import subprocess
+import re
+import os
+import tempfile
+
+SSH_OPTIONS = "-o ConnectTimeout=5"
+SSH_USER_HOST = "chatdokku@172.17.0.1"
+WORK_DIR = "/home/chatdokku/apps"
 
 # relies on 2>&1
 def result_to_dict(result):
@@ -8,10 +15,48 @@ def result_to_dict(result):
     }
 
 def exec_as_chatdokku(command):
-    ssh_command = f'ssh -o ConnectTimeout=5 chatdokku@172.17.0.1 {command} 2>&1'
+    ssh_command = f'ssh {SSH_OPTIONS} {SSH_USER_HOST} {command} 2>&1'
     result = subprocess.run(ssh_command, capture_output=True, text=True, shell=True)
     return result_to_dict(result)
 
+# runs scripts that are in ./scripts/local
 def exec_script_as_chatdokku(script):
-    return exec_as_chatdokku(f'/home/chatdokku/apps/chat-dokku-plugin/scripts/local/{script}')
+    return exec_as_chatdokku(f'{WORK_DIR}/chat-dokku-plugin/scripts/local/{script}')
+
+def is_safe_path(app_name, path):
+    # Regex to check app_name and path only contain safe characters
+    if not re.match("^[a-zA-Z0-9-]+$", app_name):
+        return False
+    if not re.match("^[a-zA-Z0-9-/.]+$", path):
+        return False
+
+    # Construct the full path
+    full_path = os.path.join(WORK_DIR, app_name, path)
+
+    # Normalize the path to resolve any '..' or '.' components
+    normalized_path = os.path.normpath(full_path)
+
+    # Check that the normalized path is still under the intended directory
+    if not normalized_path.startswith(WORK_DIR + "/"):
+        return False
+
+    return True
+
+
+def scp_to_app(file_contents, app_name, path):
+    destination = WORK_DIR + "/" + app_name + "/" + path
+
+    check_command = f'ssh {SSH_OPTIONS} {SSH_USER_HOST} test -e {destination} && echo "File exists" || echo "File does not exist"'
+    check_result = subprocess.run(check_command, capture_output=True, text=True, shell=True)
+
+    if "File exists" in check_result.stdout:
+        return {"output": "Error: File already exists", "returncode": 1}
+
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(file_contents.encode())
+        tmp_path = tmp_file.name
+
+    scp_command = f'scp {SSH_OPTIONS} {tmp_path} {SSH_USER_HOST}:{destination}'
+    result = subprocess.run(scp_command, capture_output=True, text=True, shell=True)
+    return result_to_dict(result)
 
